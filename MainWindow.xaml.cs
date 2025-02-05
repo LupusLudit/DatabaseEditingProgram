e8c26fc70fa8506ac55e3d92f0c2adf1103cc3b5
LupusLudit <michal.bielina@centrum.cz>
2025-02-05 21:38:09 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using DatabaseEditingProgram.database;
+using Microsoft.Data.SqlClient;
+using System.Configuration;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace DatabaseEditingProgram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            string server = ServerTxtField.Text;
+            string database = DatabaseTxtField.Text;
+            string username = UsernameTxtField.Text;
+            string password = PasswordBox.Password;
+
+            if (ContainsWhiteSpaces(server, database, username, password)) 
+            {
+                MessageBox.Show("Please fill in all fields", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            UpdateConfig("DataSource", server);
+            UpdateConfig("Database", database);
+            UpdateConfig("Name", username);
+            UpdateConfig("Password", password);
+
+            if (TryConnect())
+            {
+                MessageBox.Show("Connection successful", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                DatabaseWindow databaseWindow = new DatabaseWindow();
+                databaseWindow.Show();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Connection failed, try again later", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private bool TryConnect()
+        {
+            try
+            {
+                using (SqlConnection conn = DatabaseSingleton.GetInstance())
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static void UpdateConfig(string key, string newValue)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            if (config.AppSettings.Settings[key] != null)
+            {
+                config.AppSettings.Settings[key].Value = newValue;
+            }
+
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        private bool ContainsWhiteSpaces(string server, string database, string username, string password)
+        {
+            return string.IsNullOrWhiteSpace(server) || string.IsNullOrWhiteSpace(database) ||
+                string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password);
+        }
+    }
+}
---
+++ database/DatabaseSingleton.cs
@@ -0,0 +1,53 @@
+﻿using System.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace DatabaseEditingProgram.database
+{
+    public class DatabaseSingleton
+    {
+        private static SqlConnection? conn = null;
+        private DatabaseSingleton()
+        {
+
+        }
+        public static SqlConnection GetInstance()
+        {
+            if (conn == null)
+            {
+                SqlConnectionStringBuilder consStringBuilder = new SqlConnectionStringBuilder();
+                consStringBuilder.UserID = ReadSetting("Name");
+                consStringBuilder.Password = ReadSetting("Password");
+                consStringBuilder.InitialCatalog = ReadSetting("Database");
+                consStringBuilder.DataSource = ReadSetting("DataSource");
+                consStringBuilder.ConnectTimeout = 30;
+                consStringBuilder.TrustServerCertificate = true;
+
+                conn = new SqlConnection(consStringBuilder.ConnectionString);
+                Console.WriteLine(consStringBuilder.ConnectionString);
+                if (conn.State != System.Data.ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+            }
+            return conn;
+        }
+
+
+        public static void CloseConnection()
+        {
+            if (conn != null)
+            {
+                conn.Close();
+                conn.Dispose();
+                conn = null;
+            }
+        }
+
+        private static string ReadSetting(string key)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            string result = appSettings[key] ?? "Not Found";
+            return result;
+        }
+    }
+}
